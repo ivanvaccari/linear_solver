9ac1041963cecd005dfd606907ff6d906fd1da94
grisson <vaccari87@gmail.com>
2017-03-01 17:12:32 +0100

    Added comments, added license, added mini info on program start

--- include/Tokenizer.h
+++ include/Tokenizer.h
@@ -1,7 +1,28 @@
+/*
+ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ Version 2, December 2004
+
+ Copyright (C) 2017 Ivan Vaccari <vaccari87@gmail.com>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+ 0. You just DO WHAT THE FUCK YOU WANT TO.
+
+
+*/
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 #include <vector>
 #include <string>
+
+/*! \brief The tokenizer class is a multi-token tokenizer.
+           The class create a list of tokens by splitting an input string when one of the specified delimiter is found.
+           */
 
 class Tokenizer
 {
--- main.cpp
+++ main.cpp
@@ -1,16 +1,40 @@
+/*
+ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ Version 2, December 2004
+
+ Copyright (C) 2017 Ivan Vaccari <vaccari87@gmail.com>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+ 0. You just DO WHAT THE FUCK YOU WANT TO.
+
+
+*/
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <map>
+#include <cstdio>
+#include <ctime>
 #include "include/Matrix.h"
 #include "include/ColumnVector.h"
 #include "include/CommandParser.h"
 #include "include/Algorithms.h"
 
 int main(int argc, char* argv[]){
+
+    //checking command line parameters
     if (argc<2){
-        std::cerr << "Linear system " << std::endl;
-        std::cerr << "Usage: " << argv[0] << " -f datafile.txt" << std::endl;
+        std::cerr << "Linear system solver.\nA simple linear system solver for learning purposes.\n" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " -f datafile.txt\n" << std::endl;
+        std::cerr << "Parameter list\n -h  Show a small man page\n -f  Specify the input definition file\n" << std::endl;
+        std::cerr << "This version support these solving algorithms: cramer rule\n" << std::endl;
+
         return 1;
     }
 
@@ -20,10 +44,16 @@ int main(int argc, char* argv[]){
             i++;
             fileName=std::string(argv[i]);
         }
+        if (strcmp(argv[i],"-f")==0){
+            i++;
+            fileName=std::string(argv[i]);
+        }
     }
 
+    //matrix and vector cache
     std::map<std::string,Matrix> matrixes;
     std::map<std::string,ColumnVector> columnVectors;
+
     try{
         CommandParser p;
         p.loadFromFile(fileName);
@@ -31,6 +61,8 @@ int main(int argc, char* argv[]){
         if (p.operations.size()==0){
             throw std::string("No operations defined. Stopping here");
         }else{
+            //just add target vector into cache to avoid being searched into the definition file.
+            //These vector aren't defined i the file, and without this trick the program will halt
             std::list<Operation> operations=p.operations;
             while (!operations.empty()){
                 columnVectors[operations.front().target]=ColumnVector();
@@ -54,12 +86,13 @@ int main(int argc, char* argv[]){
         //loading vectors
         std::list<std::string> columnVectorNames=p.getColumnVectorNames();
         while (!columnVectorNames.empty()){
-            ColumnVector v;
             std::string colVectorName=columnVectorNames.front();
+            //in the case the vector is already  in cache, do not load it from file
             if (columnVectors.find(colVectorName)!=columnVectors.end()){
                 columnVectorNames.pop_front();
                 continue;
             }
+            ColumnVector v;
             if (!v.loadFromFile(fileName,colVectorName))
                 throw std::string("Can't load column vector ").append(colVectorName).append(". Column vector not defined");
             else
@@ -69,7 +102,6 @@ int main(int argc, char* argv[]){
         }
 
         //running commands
-
         while (!p.operations.empty()){
             Operation op=p.operations.front();
             p.operations.pop_front();
@@ -86,7 +118,9 @@ int main(int argc, char* argv[]){
         }
 
     }catch(std::string e){
+        //just show the error message before exit
         std::cout <<e;
+        return 2;
     }
     return 0;
 }
